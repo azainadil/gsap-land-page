c9f82dbe69bd930082976cd3e7aa10ef3a5e45b8
Azain Adil <axainmumtaz@gmail.com>
2024-07-05 21:01:10 +0500

    commit

---
+++ script.js
@@ -0,0 +1,25 @@
+gsap.from("#img1",{
+    opacity:0,
+    delay:0.4,
+    duration:1,
+    y:50
+}) 
+
+gsap.from("#img2",{
+    opacity:0,
+    delay:0.4,
+    duration:1,
+    x:40
+}) 
+gsap.from("#img3",{
+    opacity:0,
+    delay:0.4,
+    duration:1,
+    y:-50
+}) 
+gsap.from("#main >h1",{
+    opacity:0,
+    delay:0.4,
+    duration:1,
+    y:60
+}) 
